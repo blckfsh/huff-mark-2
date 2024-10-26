8de7a029cd7d59de521f177d12fa1c4862e7a6db
Lloyd Montero <catchphilippines@gmail.com>
2024-10-26 13:22:21 +0800

    added erc20

--- src/interfaces/IERC20.sol
+++ src/interfaces/IERC20.sol
@@ -1,89 +1,73 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.15;
 
-/**
- * @notice Interface of the ERC-20 standard as defined in the ERC.
- * @author OpenZeppelin Contracts (last updated v5.0.0) (token/ERC20/IERC20.sol)
- */
+/// @notice Interface of an ERC20 token with minting and burning functions.
+/// @author Adapted from OpenZeppelin Contracts (last updated v4.6.0) (token/ERC20/IERC20.sol)
 interface IERC20 {
-    /**
-     * @dev Emitted when `value` tokens are moved from one account (`from`) to
-     * another (`to`).
-     *
-     * Note that `value` may be zero.
-     */
+    /// @dev Emitted when `value` tokens are moved from one account (`from`) to another (`to`)
     event Transfer(address indexed from, address indexed to, uint256 value);
 
-    /**
-     * @dev Emitted when the allowance of a `spender` for an `owner` is set by
-     * a call to {approve}. `value` is the new allowance.
-     */
+    /// @dev Emitted when the allowance of a `spender` for an `owner` is set by a call to {approve}
     event Approval(
         address indexed owner,
         address indexed spender,
         uint256 value
     );
 
-    /**
-     * @dev Returns the value of tokens in existence.
-     */
+    /// @dev Returns the name of token
+    function name() external view returns (string memory);
+
+    /// @dev Returns the symbol of token
+    function symbol() external view returns (string memory);
+
+    /// @dev Returns the symbol of token
+    function decimals() external view returns (uint256);
+
+    /// @dev Returns the domain separator
+    function DOMAIN_SEPARATOR() external view returns (bytes32);
+
+    /// @dev Permit
+    function permit(
+        address,
+        address,
+        uint256,
+        uint256,
+        uint8,
+        bytes32,
+        bytes32
+    ) external;
+
+    /// @dev Permit Nonces
+    function nonces(address) external returns (uint256);
+
+    /// @dev Returns the amount of tokens in existence
     function totalSupply() external view returns (uint256);
 
-    /**
-     * @dev Returns the value of tokens owned by `account`.
-     */
+    /// @dev Returns the amount of tokens owned by `account`
     function balanceOf(address account) external view returns (uint256);
 
-    /**
-     * @dev Moves a `value` amount of tokens from the caller's account to `to`.
-     *
-     * Returns a boolean value indicating whether the operation succeeded.
-     *
-     * Emits a {Transfer} event.
-     */
-    function transfer(address to, uint256 value) external returns (bool);
+    /// @dev Creates new token.
+    function transfer(address to, uint256 amount) external returns (bool);
 
-    /**
-     * @dev Returns the remaining number of tokens that `spender` will be
-     * allowed to spend on behalf of `owner` through {transferFrom}. This is
-     * zero by default.
-     *
-     * This value changes when {approve} or {transferFrom} are called.
-     */
+    /// @dev Moves `amount` tokens from `from` to `to` using the allowance mechanism
+    function transferFrom(
+        address from,
+        address to,
+        uint256 amount
+    ) external returns (bool);
+
+    /// @dev Returns the remaining number of tokens that `spender` will be allowed to spend on behalf of `owner`
     function allowance(
         address owner,
         address spender
     ) external view returns (uint256);
 
-    /**
-     * @dev Sets a `value` amount of tokens as the allowance of `spender` over the
-     * caller's tokens.
-     *
-     * Returns a boolean value indicating whether the operation succeeded.
-     *
-     * IMPORTANT: Beware that changing an allowance with this method brings the risk
-     * that someone may use both the old and the new allowance by unfortunate
-     * transaction ordering. One possible solution to mitigate this race
-     * condition is to first reduce the spender's allowance to 0 and set the
-     * desired value afterwards:
-     * https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
-     *
-     * Emits an {Approval} event.
-     */
-    function approve(address spender, uint256 value) external returns (bool);
+    /// @dev Sets `amount` as the allowance of `spender` over the caller's tokens.
+    function approve(address spender, uint256 amount) external returns (bool);
 
-    /**
-     * @dev Moves a `value` amount of tokens from `from` to `to` using the
-     * allowance mechanism. `value` is then deducted from the caller's
-     * allowance.
-     *
-     * Returns a boolean value indicating whether the operation succeeded.
-     *
-     * Emits a {Transfer} event.
-     */
-    function transferFrom(
-        address from,
-        address to,
-        uint256 value
-    ) external returns (bool);
+    /// @dev Moves `amount` tokens from the caller's account to `to`.
+    function mint(address to, uint256 amount) external;
+
+    /// @dev Removes `amount` tokens from the `from` account.
+    function burn(address from, uint256 amount) external;
 }
